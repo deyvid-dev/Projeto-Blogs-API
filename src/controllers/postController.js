d2141ee0062d475db853b524cc938be612a6eedf
deyvid-dev <deyvidscm@hotmail.com>
2022-08-12 15:14:20 -0300

    Req-14

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -14,4 +14,16 @@ const getAll = async (req, res) => {
  return res.status(200).json(posts);
 };
 
-module.exports = { getAll };
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const post = await BlogPost.findByPk(id, {
+    include: [{ model: User, as: 'user', attributes: { exclude: 'password' } },
+    { model: Category, as: 'categories', through: { attributes: [] } }],
+  });
+  if (!post) {
+    return res.status(404).json({ message: 'Post does not exist' });
+  }
+  return res.status(200).json(post);
+};
+
+module.exports = { getAll, getById };
--- src/routes/PostRoute.js
+++ src/routes/PostRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAll } = require('../controllers/postController');
+const { getAll, getById } = require('../controllers/postController');
 const { tokenMiddleware } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', tokenMiddleware, getAll);
+router.get('/:id', tokenMiddleware, getById);
 
 module.exports = router;
